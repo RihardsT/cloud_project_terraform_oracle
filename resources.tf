2aac0a0f15231f7c6e61a1d88fe78e105e2e8747
Rihards <rudenspavasaris@proton.me>
2024-04-10 22:45:34 +0300

    Working setup - publicly available VM provisioned

--- resources.tf
+++ resources.tf
@@ -4,20 +4,47 @@ resource "oci_identity_compartment" "test_compartment" {
   name           = "rihtest"
 }
 
-data "oci_objectstorage_namespace" "test_namespace" {
-  compartment_id = oci_identity_compartment.test_compartment.id
-}
+# data "oci_objectstorage_namespace" "test_namespace" {
+#   compartment_id = oci_identity_compartment.test_compartment.id
+# }
 
-resource "oci_objectstorage_bucket" "test_bucket" {
-  compartment_id = oci_identity_compartment.test_compartment.id
-  # compartment_id = var.root_compartment
-  name      = "rih_test_storage"
-  namespace = data.oci_objectstorage_namespace.test_namespace.namespace
-}
+# resource "oci_objectstorage_bucket" "test_bucket" {
+#   compartment_id = oci_identity_compartment.test_compartment.id
+#   # compartment_id = var.root_compartment
+#   name      = "rih_test_storage"
+#   namespace = data.oci_objectstorage_namespace.test_namespace.namespace
+# }
 
 data "oci_identity_availability_domain" "test_compartment" {
     compartment_id = var.tenancy_ocid
     ad_number = 1
+}
+
+resource "oci_core_vcn" "test_vcn" {
+  compartment_id = oci_identity_compartment.test_compartment.id
+  cidr_blocks = ["10.0.0.0/16"]
+}
+
+resource "oci_core_route_table" "test_route_table" {
+  compartment_id = oci_identity_compartment.test_compartment.id
+  vcn_id = oci_core_vcn.test_vcn.id
+  route_rules {
+    network_entity_id = oci_core_internet_gateway.test_internet_gateway.id
+    destination = "0.0.0.0/0"
+    destination_type = "CIDR_BLOCK"
+  }
+}
+
+resource "oci_core_subnet" "test_subnet" {
+  cidr_block = "10.0.1.0/24"
+  compartment_id = oci_identity_compartment.test_compartment.id
+  vcn_id = oci_core_vcn.test_vcn.id
+  route_table_id = oci_core_route_table.test_route_table.id
+}
+
+resource "oci_core_internet_gateway" "test_internet_gateway" {
+  compartment_id = oci_identity_compartment.test_compartment.id
+  vcn_id = oci_core_vcn.test_vcn.id
 }
 
 resource "oci_core_instance" "test_instance" {
@@ -29,6 +56,10 @@ resource "oci_core_instance" "test_instance" {
   shape_config {
     memory_in_gbs = 24
     ocpus = 4
+  }
+  create_vnic_details {
+    assign_public_ip = "true"
+    subnet_id = oci_core_subnet.test_subnet.id
   }
   source_details {
     boot_volume_size_in_gbs = "100"
@@ -42,4 +73,8 @@ resource "oci_core_instance" "test_instance" {
     # }
   }
   metadata = var.vm_metadata
+}
+
+output "ip" {
+  value = oci_core_instance.test_instance.public_ip
 }
