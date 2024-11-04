d8105d78ec0d27eb4bbce1b058e4ca032b57299d
Rihards <rudenspavasaris@proton.me>
2024-11-04 16:52:30 +0200

    Add some more rules for private subnet.
    Actually I don't remember, if these were needed for connection between subnets

--- Base/private_subnet.tf
+++ Base/private_subnet.tf
@@ -9,9 +9,9 @@ resource "oci_core_security_list" "private_security_list" {
   compartment_id = oci_identity_compartment.rihtest.id
   vcn_id         = oci_core_vcn.test_vcn.id
   egress_security_rules {
+    protocol         = "all"
     destination      = "0.0.0.0/0"
     destination_type = "CIDR_BLOCK"
-    protocol         = "all"
     stateless        = false
   }
 
@@ -21,6 +21,27 @@ resource "oci_core_security_list" "private_security_list" {
     source_type = "CIDR_BLOCK"
     stateless   = false
   }
+  ingress_security_rules {
+    protocol    = "1"
+    source      = "10.0.0.0/16"
+    source_type = "CIDR_BLOCK"
+    stateless   = false
+    icmp_options {
+      code = -1
+      type = 3
+    }
+  }
+  ingress_security_rules {
+    protocol    = "1"
+    source      = "0.0.0.0/0"
+    source_type = "CIDR_BLOCK"
+    stateless   = false
+    icmp_options {
+      code = 4
+      type = 3
+    }
+  }
+
   timeouts {}
 }
 
